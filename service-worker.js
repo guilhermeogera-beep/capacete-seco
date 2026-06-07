bd9bc78cf9278c1d131a1fe6e50de87a6d93066e
guilhermeogera-beep <guilhermeogera@gmail.com>
2026-06-06 23:44:14 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'capacete-seco-v2';
+const CACHE_NAME = 'capacete-seco-v3';
 
 const urlsToCache = [
     './',
@@ -13,9 +13,8 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             return cache.addAll(urlsToCache);
-        })
+        }).then(() => self.skipWaiting())
     );
-    self.skipWaiting();
 });
 
 // Ativação: limpa caches antigos
